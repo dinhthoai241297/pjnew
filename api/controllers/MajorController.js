78a839ec856f73c56b49106c5fadf712fcb50a17
Pham Dinh Thoai <15130180@st.hcmuaf.edu.vn>
2018-08-23 09:50:37 +0700

    edit controller Major

--- api/controllers/MajorController.js
+++ api/controllers/MajorController.js
@@ -6,58 +6,103 @@
  */
 
 module.exports = {
+
+    // 415 invalid data
+    // 200 success
+    // 304 not modified
+    // 404 not found data
+
     add: async (req, res) => {
+        res.status(200);
+        let code = 404, data = null, message = 'error';
         try {
             let major = JSON.parse(req.param('data'));
             let school = await School.findOne({ id: mark.school });
             if (school) {
                 let s = await Major.create(major).fetch();
-                return res.status(s ? 200 : 304).send();
-            } else {
-                return res.status(404).send();
+                if (s) {
+                    code = 200;
+                    message = 'success';
+                    data = s;
+                } else {
+                    code = 304;
+                }
             }
         } catch (error) {
-            res.status(500).send();
+            code = 415;
         }
+        res.json({ code: code, message: message, data: data });
     },
 
     delete: async (req, res) => {
+        res.status(200);
+        let code = 404, data = null, message = 'error';
         let id = req.param('id');
         if (id) {
             let rs = await Major.destroy({ id: id }).fetch();
-            return res.status((rs && rs.length !== 0) ? 200 : 304).send();
-        } else {
-            return res.status(500).send();
+            if (rs && rs.length !== 0) {
+                code = 200;
+                message = 'success';
+                data = rs;
+            } else {
+                code = 304;
+            }
         }
+        res.json({ code: code, message: message, data: data });
     },
 
     // t
     update: async (req, res) => {
+        res.status(200);
+        let code = 404, data = null, message = 'error';
         try {
             let major = JSON.parse(req.param('data'));
             let school = await School.findOne({ id: mark.school });
             if (school) {
-                let s = await Major.update({ id: major.id }, major).fetch();
-                return res.status(s ? 200 : 304).send();
-            } else {
-                return res.status(404).send();
+                let s = await Major.update({id: school.id}, major).fetch();
+                if (s) {
+                    code = 200;
+                    message = 'success';
+                    data = s;
+                } else {
+                    code = 304;
+                }
             }
         } catch (error) {
-            res.status(500).send();
+            code = 415;
         }
+        res.json({ code: code, message: message, data: data });
     },
 
     // /major/getall/:page
     getAll: async (req, res) => {
-        let page = req.param('page') || 1;
-        let list = await Major.find().limit(10).skip((page - 1) * 10);
-        return rs.send(list);
+        res.status(200);
+        let code = 200, data = null, message = 'success', page = req.param('page') || 1;
+        let list = await Major.find().limit(11).skip((page - 1) * 10);
+        if (list.length > 10) {
+            data = {
+                list: list.splice(0, 10),
+                next: true
+            }
+        } else {
+            data = {
+                list: list,
+                next: false
+            }
+        }
+        res.json({ code: code, message: message, data: data });
     },
 
     // /major/getone/:id
     getOne: async (req, res) => {
-        let id = req.param('id') || 1;
-        let major = await Major.find({ id: id });
+        res.status(200);
+        let code = 404, data = null, message = 'error', id = req.param('id') || -1;
+        let major = await Major.findOne({ id: id });
+        if (major) {
+            code = 200;
+            data = major;
+            message = 'success';
+        }
         return res.send(major);
     }
 
