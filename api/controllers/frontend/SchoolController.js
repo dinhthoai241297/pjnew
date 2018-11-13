7205eb0f98e611dfcb38fac017e5b95291bd2973
Pham Dinh Thoai <15130180@st.hcmuaf.edu.vn>
2018-11-13 10:09:54 +0700

    'school/searcg': điều chỉnh điều kiện lọc khi không có tham số gửi lên

--- api/controllers/frontend/SchoolController.js
+++ api/controllers/frontend/SchoolController.js
@@ -5,7 +5,7 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
- module.exports = {
+module.exports = {
 
     // 301 dữ liệu gửi lên không hợp lệ
     // 302 có lỗi xảy ra, không có gì được thay đổi
@@ -16,30 +16,41 @@
     getId: async (req, res) => {
         res.status(200);
         let code = 303, message = 'error', data = undefined, { id } = req.param('data');
-        data = await School.findOne({ id: id });
+        data = await School.findOne({ id: id }).populate('province');
         if (data) {
             code = 200;
             message = 'success';
         }
         return res.json({ code, message, data });
     },
-    
-   
+
+
     // /school/search
     search: async (req, res) => {
         res.status(200);
-        let code = 303, message = 'error', data = undefined, { name } = req.param('data') || 1;
-        list = await School.find().where({ or: [ {name: {contains :name}}, {code: {contains : name}}]}).sort([{name :'ASC'}]).limit(11).skip((page - 1) * 10);
-        if (list.length > 10) {
-            data = {
-                list: list.slice(0, 10),
-                next: true
+        let code = 303, message = 'error', data = undefined, { name, page = 1 } = req.param('data');
+        try {
+            console.log(name, page);
+            let criteria = {};
+            if (name) {
+                criteria.or = [{ name: { 'contains': name } }, { code: { 'contains': name } }];
             }
-        } else {
-            data = {
-                list,
-                next: false
+            list = await School.find().where(criteria).populate('province').sort([{ name: 'ASC' }]).limit(11).skip((page - 1) * 10);
+            if (list.length > 10) {
+                data = {
+                    list: list.slice(0, 10),
+                    next: true
+                }
+            } else {
+                data = {
+                    list,
+                    next: false
+                }
             }
+            code = 200;
+            message = 'success';
+        } catch (error) {
+            code = 301;
         }
         return res.json({ code, message, data });
     },
