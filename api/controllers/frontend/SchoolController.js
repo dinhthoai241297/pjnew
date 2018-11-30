7a00c93b3e3fa7800fc57564220c8a1a2e4eafc9
Pham Dinh Thoai <15130180@st.hcmuaf.edu.vn>
2018-11-30 08:22:47 +0700

    cập nhật limit school

--- api/controllers/frontend/SchoolController.js
+++ api/controllers/frontend/SchoolController.js
@@ -5,7 +5,7 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
- module.exports = {
+module.exports = {
 
     // 301 dữ liệu gửi lên không hợp lệ
     // 302 có lỗi xảy ra, không có gì được thay đổi
@@ -34,28 +34,28 @@
         try {
             let db = School.getDatastore().manager;
             list = await db.collection('school').aggregate([
-            {
-                $match: {
-                    $or: [
-                    {
+                {
+                    $match: {
                         $or: [
-                        { code: { $regex: name, $options: "i" } },
-                        { name: { $regex: name, $options: "i" } }
+                            {
+                                $or: [
+                                    { code: { $regex: name, $options: "i" } },
+                                    { name: { $regex: name, $options: "i" } }
+                                ]
+                            }
                         ]
                     }
-                    ]
-                }
-            },
-            {
-                $lookup: {
-                    from: 'province',
-                    localField: 'province',
-                    foreignField: '_id',
-                    as: 'province'
-                }
-            },
-            { $skip: (page - 1) * 20 },
-            { $limit: 20 }
+                },
+                {
+                    $lookup: {
+                        from: 'province',
+                        localField: 'province',
+                        foreignField: '_id',
+                        as: 'province'
+                    }
+                },
+                { $skip: (page - 1) * 20 },
+                { $limit: 21 }
             ]).toArray((error, rs) => {
                 if (!error) {
                     list = rs;
@@ -83,53 +83,53 @@
 
 
 
-     // /school/getList/:province + subjectGroup
+    // /school/getList/:province + subjectGroup
 
 
-     getList: async (req, res) => {
+    getList: async (req, res) => {
         res.status(200);
         let code = 200, message = 'success', data = undefined, { page, province, subjectGroups } = req.param('data');
         if (!page || page < 0) {
             page = 1;
         }
         // let listmark = await Mark.find({ subjectGroups : subjectGroups });
-         // for(let i = 0; i < listmark.length ; i++){
-            // let listsc = listmark[i];
-            // let listidsc = listsc.school;
-            // console.log(listidsc);
-            // list = await School.find({province : province ,id: {in :[listidsc]}});
-            // console.log(list);
-            // if (list.length > 10) {
-            //     data = {
-            //         list: list.slice(0, 10),
-            //         next: true
-            //     }
-            // } else {
-            //     data = {
-            //         list,
-            //         next: false
-            //     }
-            // }
-            // return res.json({ code, message, data });
-            let listin = await School.find({province : province});
-            let listnin = await School.find({province : {nin :[province]}});
-            let list = listin && listnin;
-            if (list.length > 20) {
-                data = {
-                    listin,
-                    listnin : listnin.slice(listin.length, 20),
-                    next: true
-                }
-            } else {
-                data = {
-                    list,
-                    next: false
-                }
+        // for(let i = 0; i < listmark.length ; i++){
+        // let listsc = listmark[i];
+        // let listidsc = listsc.school;
+        // console.log(listidsc);
+        // list = await School.find({province : province ,id: {in :[listidsc]}});
+        // console.log(list);
+        // if (list.length > 10) {
+        //     data = {
+        //         list: list.slice(0, 10),
+        //         next: true
+        //     }
+        // } else {
+        //     data = {
+        //         list,
+        //         next: false
+        //     }
+        // }
+        // return res.json({ code, message, data });
+        let listin = await School.find({ province: province });
+        let listnin = await School.find({ province: { nin: [province] } });
+        let list = listin && listnin;
+        if (list.length > 20) {
+            data = {
+                listin,
+                listnin: listnin.slice(listin.length, 20),
+                next: true
             }
-            return res.json({ code, message, data });
+        } else {
+            data = {
+                list,
+                next: false
+            }
         }
+        return res.json({ code, message, data });
+    }
 
 
-    };
+};
 
 
