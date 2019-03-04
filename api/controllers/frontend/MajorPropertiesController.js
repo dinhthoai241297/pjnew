8e1019207bb741dd07aa0c914c764751eb0fa1eb
= <=>
2019-03-04 15:31:40 +0700

    fix phân trang api majorproperties

--- api/controllers/frontend/MajorPropertiesController.js
+++ api/controllers/frontend/MajorPropertiesController.js
@@ -14,17 +14,13 @@ module.exports = {
         if (!page || page < 0) {
             page = 1;
         }
-        let list = await GroupMajor.find().sort([{ name: 'DESC' }]).limit(11).skip((page - 1) * 10).populate('groupmajoritem');
-        if (list.length > 10) {
-            data = {
-                list: list.slice(0, 10),
-                next: true
+        let list = await GroupMajor.find().sort([{ name: 'DESC' }]).populate('groupmajoritem');
+        if (list) {
+            data ={
+                list
             }
-        } else {
-            data = {
-                list,
-                next: false
-            }
+            code = 200;
+            message = 'success';
         }
         return res.json({ code, message, data });
     },
